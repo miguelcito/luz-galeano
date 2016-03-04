19ae909e93ca7dca4b56bdd379309c1ae7517036
miguel <espinoza@12>
2016-03-03 21:11:09 -0500

    luz galeano

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>LUZ GALEANO </title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<h>hola amia </h>
+<h>hola amio </h>
+<h>como estas amia luz</h>
+<h>todo bn gracias amito y tu</h>
+<h>tambn bien noma amita gracias </h>	
+<<img src="daysi.gif" alt="">
+<<img src="mmm.gif" alt="">
+</body>
+</html>
